fd43115d86e573a6b13d41b56d2f92d133c3f141
VOLGA SENATAR <106486711+volhasenatar@users.noreply.github.com>
2023-01-27 10:21:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+const button = document.querySelector('button');
+const playForest = document.querySelector('.play');
+const main = document.querySelector('.main');
+const active = document.querySelectorAll('li');
+
+const imageArray = [
+  'url(img/solovey.jpg)',
+  'url(img/zarynka.jpg)',
+  'url(img/javoronok.jpg)',
+  'url(img/slavka.jpg)',
+  'url(img/drozd.jpg)',
+];
+
+document.querySelector('.logo').addEventListener('click', () => {
+  window.location.reload();
+});
+
+const audio = new Audio();
+const playlist = new Array(
+  'audio/forest.mp3',
+  'audio/solovey.mp3',
+  'audio/zarynka.mp3',
+  'audio/javoronok.mp3',
+  'audio/slavka.mp3',
+  'audio/drozd.mp3'
+);
+
+function playMusic() {
+  if (audio.paused === true) {
+    audio.src = playlist[0];
+    audio.play();
+    audio.currentTime = 0;
+  } else {
+    audio.pause();
+  }
+}
+function toggleBtn() {
+  button.classList.toggle('pause');
+}
+
+playForest.addEventListener('click', playMusic);
+button.addEventListener('click', toggleBtn);
+
+//solovey
+active[0].addEventListener('click', () => {
+  main.style.backgroundImage = imageArray[0];
+});
+//zarynka
+active[1].addEventListener('click', () => {
+  main.style.backgroundImage = imageArray[1];
+});
+
+//javoronok
+active[2].addEventListener('click', () => {
+  main.style.backgroundImage = imageArray[2];
+});
+
+//slavka
+active[3].addEventListener('click', () => {
+  main.style.backgroundImage = imageArray[3];
+});
+
+//drozd
+active[4].addEventListener('click', () => {
+  main.style.backgroundImage = imageArray[4];
+});
+
+for (i = 0; i < active.length; i++)
+  active[i].onclick = function () {
+    this.classList.add('active');
+  };
+
+// если при нажатии 1  из li актиыируется активный класс. то остальные автоматически не имеют активный класс
