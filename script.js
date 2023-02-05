0f6bef204b6ba3eaa679e84c61bfea2cd10493c2
Olga Senator <106486711+volhasenatar@users.noreply.github.com>
2023-02-05 01:27:18 +0100

    final :)

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-import { active } from './constants.js';
-import { changeActiveLinks } from './changeActiveLinks.js';
-import { changeIndex } from './changeIndex.js';
+import { active } from './javaScript-s/constants.js';
+import { changeActiveLinks } from './javaScript-s/changeActiveLinks.js';
+import { changeIndex } from './javaScript-s/changeIndex.js';
+import { audio, playlist } from './javaScript-s/arrays.js';
 
 active.forEach(function (element, index) {
+  if (element.classList.contains('active')) {
+    audio.src = playlist[0];
+    audio.play();
+  }
   element.onclick = () => {
     changeIndex(index);
     changeActiveLinks(element);
